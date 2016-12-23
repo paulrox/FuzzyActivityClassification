302fb8a5abbd59b43fb4c9128d319a7b6fe24649
Gyro <rotundo.matteo@gmail.com>
2016-12-23 15:42:42 +0100

    features done: bandwidth and max peak

--- dataInTimeDomain.m
+++ dataInTimeDomain.m
@@ -1,5 +1,5 @@
 function [ x, y ] = dataInTimeDomain(data, i, j, k)
-% It returns the frequency and the fft of the 
+% It returns the timestamp and the output of the 
 % activity i of patient j by sensor k from the dataset
     
     AiPj = data{i,j};
