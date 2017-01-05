f54975c5b40036b509caaf96beee42951b096734
Gyro <rotundo.matteo@gmail.com>
2017-01-05 16:51:29 +0100

    added classifiers window 2

--- extract_features_windowed.m
+++ extract_features_windowed.m
@@ -6,14 +6,13 @@
 
 feat_temp = extract_features2(sensor_filtered,1/82e-3)';
 %%
-sensor = 2;
-window = 1;
+window = 2;
 feat = 17;
 
-label = 1
+label = 1;
 
 
-for sensor=1:2
+for sensor=2:3
 features1 = add_label2(feat_temp{feat, 1}{window, sensor}, label);
 plot(features1(1,1), features1(2,1))
 
@@ -38,23 +37,3 @@ for i=1:1:length(features1)
 end
 end
     
-
-% feature_band1 = extract_bandwidth_windowed( data, 1, 1, Fs, window )
-% feature_band2 = extract_bandwidth_windowed( data, 2, 1, Fs, window )   
-% feature_band3 = extract_bandwidth_windowed( data, 3, 1, Fs, window )  
-% feature_band4 = extract_bandwidth_windowed( data, 4, 1, Fs, window )  
-% 
-% feature_band1 = add_label(feature_band1, 1);
-% feature_band2 = add_label(feature_band2, 2);
-% feature_band3 = add_label(feature_band3, 3);
-% feature_band4 = add_label(feature_band4, 4);
-% 
-% 
-% hold on
-% 
-% for i=1:1:10  
-%         plot(feature_band1{i}(:,1), feature_band1{i}(:,2), 'k*')
-%         plot(feature_band2{i}(:,1), feature_band2{i}(:,2), 'bo')
-%         plot(feature_band3{i}(:,1), feature_band3{i}(:,2), 'gx')
-%         plot(feature_band4{i}(:,1), feature_band4{i}(:,2), 'rs')
-% end
--- mamdani_window3.m
+++ mamdani_window3.m
@@ -13,35 +13,37 @@ f2 = 5;
 f3 = 11;
 f4 = 17;
 
- %% Mamdani Classifier AllvsAll
- %  Test Window 1 Sensor 1
- 
- d1 = cell2mat(features_raw{f1,1}(1,1));
- d2 = cell2mat(features_raw{f2,1}(1,1));
- d3 = cell2mat(features_raw{f3,1}(1,1));
- d4 = cell2mat(features_raw{f4,1}(1,1));
+ d1 = cell2mat(features_raw{f1,1}(3,1));
+ d2 = cell2mat(features_raw{f2,1}(3,1));
+ d3 = cell2mat(features_raw{f3,1}(3,1));
+ d4 = cell2mat(features_raw{f4,1}(3,1));
  
 start = 1;
-fine = 40;
+fine = 160;
+
+%% Mamdani Classifier AllvsAll 
+ %  Test Window 2 Sensor 1
+  
+ 
 TP = 0;
  for i = start:fine
  
     input = [d1(1,i) d2(1,i) d3(1,i) d4(1,i)];      
  
-    output1 = evalfis(input, MamdaniAllvsAll);
-    if (i >= 1 && i <=10)
+    output1 = evalfis(input, MamdaniAllvsAll_WINDOW2);
+    if (i >= 1 && i <=40)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 11 && i <=20)
+    elseif (i >= 41 && i <=80)
         if output1 > 1 && output1 <=2
             TP = TP +1;
         end
-    elseif (i >= 21 && i <=30)
+    elseif (i >= 81 && i <=120)
          if output1 > 2 && output1 <=3
             TP = TP +1;
         end
-    elseif (i >= 31 && i <=40)
+    elseif (i >= 121 && i <=160)
            if output1 > 3 && output1 <=4
             TP = TP +1;
         end 
@@ -53,39 +55,31 @@ TP = 0;
  
  end
  
- TPR_Mamdani_All_vs_All = TP / 40
- FNR_Mamdani_All_vs_All = 1 - TPR_Mamdani_All_vs_All
+ TPR_Mamdani_All_vs_All_WIN3 = TP / 160
+ FNR_Mamdani_All_vs_All_WIN3 = 1 - TPR_Mamdani_All_vs_All_WIN3
 
+%% Mamdani Classifier AlvsAll 
+ %  Test Window 2 Sensor 1
  
- %% Mamdani Classifier A1vsAll
- %  Test Window 1 Sensor 1
- 
- d1 = cell2mat(features_raw{f1,1}(1,1));
- d2 = cell2mat(features_raw{f2,1}(1,1));
- d3 = cell2mat(features_raw{f3,1}(1,1));
- d4 = cell2mat(features_raw{f4,1}(1,1));
- 
-start = 1;
-fine = 40;
-TP = 0;
+ TP = 0;
  for i = start:fine
  
     input = [d1(1,i) d2(1,i) d3(1,i) d4(1,i)];       
  
     output1 = evalfis(input,MamdaniA1vsAll);
-    if (i >= 1 && i <=10)
+    if (i >= 1 && i <=40)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 11 && i <=20)
+    elseif (i >= 41 && i <=80)
         if output1 > 1 && output1 <=2
             TP = TP +1;
         end
-    elseif (i >= 21 && i <=30)
+    elseif (i >= 81 && i <=120)
          if output1 > 1 && output1 <=2
             TP = TP +1;
         end
-    elseif (i >= 31 && i <=40)
+    elseif (i >= 121 && i <=160)
            if output1 > 1 && output1 <=2
             TP = TP +1;
         end 
@@ -96,40 +90,32 @@ TP = 0;
     plot(i, output1, 'gx')
  
  end
- 
- TPR_Mamdani_A1_vs_All = TP / 40
- FNR_Mamdani_A1_vs_All = 1 - TPR_Mamdani_A1_vs_All
 
+ TPR_Mamdani_A1_vs_All_WIN3 = TP / 160
+ FNR_Mamdani_A1_vs_All_WIN3 = 1 - TPR_Mamdani_A1_vs_All_WIN3
  
  %% Mamdani Classifier A2vsAll
- %  Test Window 1 Sensor 1
+ %  Test Window 2 Sensor 1
  
- d1 = cell2mat(features_raw{f1,1}(1,1));
- d2 = cell2mat(features_raw{f2,1}(1,1));
- d3 = cell2mat(features_raw{f3,1}(1,1));
- d4 = cell2mat(features_raw{f4,1}(1,1));
- 
-start = 1;
-fine = 40;
 TP = 0;
  for i = start:fine
  
     input = [d1(1,i) d2(1,i) d3(1,i) d4(1,i)];       
  
     output1 = evalfis(input,MamdaniA2vsAll);
-    if (i >= 1 && i <=10)
+    if (i >= 1 && i <=40)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 11 && i <=20)
+    elseif (i >= 41 && i <=80)
         if output1 > 1 && output1 <=2
             TP = TP +1;
         end
-    elseif (i >= 21 && i <=30)
+    elseif (i >= 81 && i <=120)
          if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 31 && i <=40)
+    elseif (i >= 121 && i <=160)
            if output1 > 0 && output1 <=1
             TP = TP +1;
         end 
@@ -141,37 +127,31 @@ TP = 0;
  
  end
  
- TPR_Mamdani_A2_vs_All = TP / 40
- FNR_Mamdani_A2_vs_All = 1 - TPR_Mamdani_A2_vs_All
+ TPR_Mamdani_A2_vs_All_WIN3 = TP / 160
+ FNR_Mamdani_A2_vs_All_WIN3 = 1 - TPR_Mamdani_A2_vs_All_WIN3
+
  %% Mamdani Classifier A3vsAll
- %  Test Window 1 Sensor 1
+ %  Test Window 2 Sensor 1
  
- d1 = cell2mat(features_raw{f1,1}(1,1));
- d2 = cell2mat(features_raw{f2,1}(1,1));
- d3 = cell2mat(features_raw{f3,1}(1,1));
- d4 = cell2mat(features_raw{f4,1}(1,1));
- 
-start = 1;
-fine = 40;
-TP = 0;
+ TP = 0;
  for i = start:fine
  
     input = [d1(1,i) d2(1,i) d3(1,i) d4(1,i)];       
  
     output1 = evalfis(input, MamdaniA3vsAll);
-    if (i >= 1 && i <=10)
+    if (i >= 1 && i <=40)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 11 && i <=20)
+    elseif (i >= 41 && i <=80)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 21 && i <=30)
+    elseif (i >= 81 && i <=120)
          if output1 > 2 && output1 <=3
             TP = TP +1;
         end
-    elseif (i >= 31 && i <=40)
+    elseif (i >= 121 && i <=160)
            if output1 > 0 && output1 <=1
             TP = TP +1;
         end 
@@ -183,39 +163,32 @@ TP = 0;
  
  end
  
- TPR_Mamdani_A3_vs_All = TP / 40
- FNR_Mamdani_A3_vs_All = 1 - TPR_Mamdani_A3_vs_All
+ TPR_Mamdani_A3_vs_All_WIN3 = TP / 80
+ FNR_Mamdani_A3_vs_All_WIN3 = 1 - TPR_Mamdani_A3_vs_All_WIN3
  
  
  %% Mamdani Classifier A4vsAll
- %  Test Window 1 Sensor 1
+ %  Test Window 2 Sensor 1
  
- d1 = cell2mat(features_raw{f1,1}(1,1));
- d2 = cell2mat(features_raw{f2,1}(1,1));
- d3 = cell2mat(features_raw{f3,1}(1,1));
- d4 = cell2mat(features_raw{f4,1}(1,1));
- 
-start = 1;
-fine = 40;
 TP = 0;
  for i = start:fine
  
     input = [d1(1,i) d2(1,i) d3(1,i) d4(1,i)];       
  
     output1 = evalfis(input,MamdaniA4vsAll);
-    if (i >= 1 && i <=10)
+    if (i >= 1 && i <=40)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 11 && i <=20)
+    elseif (i >= 41 && i <=80)
         if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 21 && i <=30)
+    elseif (i >= 81 && i <=120)
          if output1 > 0 && output1 <=1
             TP = TP +1;
         end
-    elseif (i >= 31 && i <=40)
+    elseif (i >= 121 && i <=160)
            if output1 > 3 && output1 <=4
             TP = TP +1;
         end 
@@ -227,7 +200,6 @@ TP = 0;
  
  end
  
- TPR_Mamdani_A4_vs_All = TP / 40
- FNR_Mamdani_A4_vs_All = 1 - TPR_Mamdani_A4_vs_All
- 
+ TPR_Mamdani_A4_vs_All_WIN3 = TP / 160
+ FNR_Mamdani_A4_vs_All_WIN3 = 1 - TPR_Mamdani_A4_vs_All_WIN3
  
