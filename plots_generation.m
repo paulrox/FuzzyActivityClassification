ae5b119dc28c27e9a0c78b6d89775725db3f394b
Paolo Sassi <paolo.sassi91@gmail.com>
2017-01-07 17:45:42 +0100

    added plot script

---
+++ plots_generation.m
@@ -0,0 +1,76 @@
+%% Computational Intelligence Project 2016
+%  Activity recognition using fuzzy classifiers.
+%
+%  A.Y.: 2016/2017 
+%  Authors: Paolo Sassi - Matteo Rotundo
+%  
+%  Script for plots extraction.
+%
+%--------------------------------------------------------------------------
+clearvars; clc; close all;
+% Load the workspace.
+load('project_workspace');
+
+
+%% GA plots.
+
+for i=1:3
+    figure;
+    plot(pat_nets{i,2}.gen,pat_nets{i,2}.mean,'.b');
+    hold on;
+    plot(pat_nets{i,2}.gen,pat_nets{i,2}.best,'.k');
+end;
+%% Sugeno FIS 1 vs. All Plots.
+
+for i=1:4
+    % Cheking error.
+    figure;
+    plot(sugeno_fis{1}{i,2}.chkErr);
+    title(['Sugeno FIS Checking Error Activity ' num2str(i) ' vs. All']);
+    xlabel('Epoch');
+    ylabel('Error');
+    
+    % Training error.
+    figure;
+    plot(sugeno_fis{1}{i,2}.error);
+    title(['Sugeno FIS Training Error Activity ' num2str(i) ' vs. All']);
+    xlabel('Epoch');
+    ylabel('Error');
+    
+    % Testing error.
+    figure;
+    plot(sugeno_outputs{1}{i,1}, '*r');
+    title(['Sugeno FIS Output Error Activity ' num2str(i) ' vs. All']);
+    xlabel('Input Number');
+    ylabel('Value');
+    hold on;
+    plot(anfis_test{1,i}(:,5), 'ob');
+    plot(sugeno_outputs{1}{i,2}, 'xk');
+    legend('FIS output', 'Target', 'Filtered output');
+end;
+
+%% Sugeno FIS All vs. All Plots.
+
+figure;
+plot(sugeno_fis{1}{5,2}.chkErr);
+title(['Sugeno FIS Checking Error Activity ' num2str(i) ' vs. All']);
+xlabel('Epoch');
+ylabel('Error');
+
+% Training error.
+figure;
+plot(sugeno_fis{1}{5,2}.error);
+title(['Sugeno FIS Training Error Activity ' num2str(i) ' vs. All']);
+xlabel('Epoch');
+ylabel('Error');
+
+% Testing error.
+figure;
+plot(sugeno_outputs{1}{5,1}, '*r');
+title(['Sugeno FIS Output Error Activity ' num2str(i) ' vs. All']);
+xlabel('Input Number');
+ylabel('Value');
+hold on;
+plot(anfis_test{1,5}(:,5), 'ob');
+plot(sugeno_outputs{1}{5,2}, 'xk');
+legend('FIS output', 'Target', 'Filtered output');
